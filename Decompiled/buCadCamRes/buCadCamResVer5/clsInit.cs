d74bd66af2a30e2806dd1a7474bab286f5e4c55c
ercankaymaz <mesteden@gmail.com>
2026-08-12 03:26:53 +0300

    Initialize the v2 editor service

--- Decompiled/buCadCamRes/buCadCamResVer5/clsInit.cs
+++ Decompiled/buCadCamRes/buCadCamResVer5/clsInit.cs
@@ -1,3 +1,9 @@
+﻿// Decompiled with JetBrains decompiler
+// Type: buCadCamResVer5.clsInit
+// Assembly: buCadCamRes, Version=5.1.1.1, Culture=neutral, PublicKeyToken=null
+// MVID: E90DB10B-14C6-404B-A6C3-E5D8AB0844F3
+// Assembly location: C:\Users\ERCAN\Desktop\CMDMarbleCNC\buCadCamRes.dll
+
 using buCadCamResVer5.Cutter;
 using buCadCamResVer5.Diemaker;
 using buCadCamResVer5.Door;
@@ -28,171 +34,97 @@ using buCore;
 using buCore.AppCalc;
 using buEyeBaseVer5;
 using buEyeBaseVer5.Apps;
-using buEyeBaseVer5.Apps.Marble;
 using buEyeBaseVer5.Apps.PanelCut;
 using buEyeBaseVer5.Apps.Robotic;
 using buEyeBaseVer5.Flexo;
 using buMW;
 
+#nullable disable
 namespace buCadCamResVer5;
 
 public class clsInit
 {
-	public static clsFiles appFiles = null;
-
-	public static clsCommand appCommand = null;
-
-	public static clsSystem appSystem = null;
-
-	public static clsMW appMW = null;
-
-	public static clsJewel appJewel = null;
-
-	public static clsSpinning appSpinning = null;
-
-	public static clsDiemaker appDiemaker = null;
-
-	public static clsLaserRouter appLaserRouter = null;
-
-	public static clsPipeBending appPipeBending = null;
-
-	public static clsRobotic appRobotic = null;
-
-	public static clsCutter appCutter = null;
-
-	public static clsTufting appTufting = null;
-
-	public static clsQuilting appQuilting = null;
-
-	public static clsMarble appMarble = null;
-
-	public static clsProfile appProfile = null;
-
-	public static clsDrill appDrill = null;
-
-	public static clsFlexo appFlexo = null;
-
-	public static clsSewing appSewing = null;
-
-	public static clsFoamCutting appFoamCutting = null;
-
-	public static clsPanelCut appPanelCut = null;
-
-	public static clsPrinter3D appPrinter3D = null;
-
-	public static clsWood appWood = null;
-
-	public static clsDoor appDoor = null;
-
-	public static clsEditor appEditor = new clsEditor();
-
-	public static clsEditorV2 appEditor2 = null;
-
-	public static clsRouter3AX appRouter3AX = null;
-
-	public static clsRollerBend appRollerBend = null;
-
-	public static clsToolGrinding appToolGrind = null;
-
-	public static clsNesting appNesting = null;
-
-	public static clsPowerNest appNestingPower = null;
-
-	public static clsNestOpaline appNestingPanel = null;
-
-	public static clsLibrary appLibrary = new clsLibrary();
-
-	public static buMWCalcs cMwCalc = null;
-
-	public static buConversion cConv = null;
-
-	public static buFile cFile = null;
-
-	public static buVector cVector = null;
-
-	public static buCamCalc cCam = null;
-
-	public static buKinematic cKinematic = null;
-
-	public static buCall cCall = null;
-
-	public static buTuftingCalc cTuft = null;
-
-	public static buQuiltingCalc cQuilt = null;
-
-	public static buCutterCalc cCutter = null;
-
-	public static buEyeBaseVer5.Apps.Marble.buMarbleCalc cMarble = null;
-
-	public static buJewelCalc cJewel = null;
-
-	public static buProfileCalc cProfile = null;
-
-	public static buFlexoCalc cFlexo = null;
-
-	public static buNestingCalc cNesting = null;
-
-	public static buDrillCalc cDrill = null;
-
-	public static buSewingCalc cSewing = null;
-
-	public static buFoamCalc cFoamCut = null;
-
-	public static buPipeBendCalc cPipeBend = null;
-
-	public static buPrinter3D cPrinter3D = null;
-
-	public static buRouter3AX cRouter3AX = null;
-
-	public static buRollerBendCalc cRollerBend = null;
-
-	public static buToolGrindingCalc cToolGrind = null;
-
-	public static buWood cWood = null;
-
-	public static buDoor cDoor = null;
-
-	public static buRoboticCalc cRobotic = null;
-
-	public static buPanelCutCalc cPanelCut = null;
-
-	public static buDiamakerCalc cDiamaker = null;
-
-	public static buAppCalc cAppCalc = null;
-
-	public static buSort cSort = null;
-
-	public static buNet cNet = null;
-
-	public static buString cString = null;
-
-	public static buGeneral cGeneral = null;
-
-	public static buNumeric cNumeric = null;
-
-	public static buImage cImage = null;
-
-	public static buMatrix cMatrix = null;
-
-	public static buGCodeCreate cGcodeCreate = null;
-
-	public static buCam5 cCam5 = null;
-
-	public static buVector5 cVector5 = null;
-
-	public static buFile5 cFile5 = null;
-
-	public static buKinematic5 cKinematic5 = null;
-
-	public static buMatrix5 cMatrix5 = null;
-
-	public static buControlCommands cControl = null;
-
-	public static void Init()
-	{
-		appFiles = new clsFiles();
-		appCommand = new clsCommand();
-		appSystem = new clsSystem();
-		new buControlCommands();
-	}
+  public static clsFiles appFiles = (clsFiles) null;
+  public static clsCommand appCommand = (clsCommand) null;
+  public static clsSystem appSystem = (clsSystem) null;
+  public static clsMW appMW = (clsMW) null;
+  public static clsJewel appJewel = (clsJewel) null;
+  public static clsSpinning appSpinning = (clsSpinning) null;
+  public static clsDiemaker appDiemaker = (clsDiemaker) null;
+  public static clsLaserRouter appLaserRouter = (clsLaserRouter) null;
+  public static clsPipeBending appPipeBending = (clsPipeBending) null;
+  public static clsRobotic appRobotic = (clsRobotic) null;
+  public static clsCutter appCutter = (clsCutter) null;
+  public static clsTufting appTufting = (clsTufting) null;
+  public static clsQuilting appQuilting = (clsQuilting) null;
+  public static clsMarble appMarble = (clsMarble) null;
+  public static clsProfile appProfile = (clsProfile) null;
+  public static clsDrill appDrill = (clsDrill) null;
+  public static clsFlexo appFlexo = (clsFlexo) null;
+  public static clsSewing appSewing = (clsSewing) null;
+  public static clsFoamCutting appFoamCutting = (clsFoamCutting) null;
+  public static clsPanelCut appPanelCut = (clsPanelCut) null;
+  public static clsPrinter3D appPrinter3D = (clsPrinter3D) null;
+  public static clsWood appWood = (clsWood) null;
+  public static clsDoor appDoor = (clsDoor) null;
+  public static clsEditor appEditor = new clsEditor();
+  public static clsEditorV2 appEditor2 = new clsEditorV2();
+  public static clsRouter3AX appRouter3AX = (clsRouter3AX) null;
+  public static clsRollerBend appRollerBend = (clsRollerBend) null;
+  public static clsToolGrinding appToolGrind = (clsToolGrinding) null;
+  public static clsNesting appNesting = (clsNesting) null;
+  public static clsPowerNest appNestingPower = (clsPowerNest) null;
+  public static clsNestOpaline appNestingPanel = (clsNestOpaline) null;
+  public static clsLibrary appLibrary = new clsLibrary();
+  public static buMWCalcs cMwCalc = (buMWCalcs) null;
+  public static buConversion cConv = (buConversion) null;
+  public static buFile cFile = (buFile) null;
+  public static buVector cVector = (buVector) null;
+  public static buCamCalc cCam = (buCamCalc) null;
+  public static buKinematic cKinematic = (buKinematic) null;
+  public static buCall cCall = (buCall) null;
+  public static buTuftingCalc cTuft = (buTuftingCalc) null;
+  public static buQuiltingCalc cQuilt = (buQuiltingCalc) null;
+  public static buCutterCalc cCutter = (buCutterCalc) null;
+  public static buEyeBaseVer5.Apps.Marble.buMarbleCalc cMarble = (buEyeBaseVer5.Apps.Marble.buMarbleCalc) null;
+  public static buJewelCalc cJewel = (buJewelCalc) null;
+  public static buEyeBaseVer5.Apps.buProfileCalc cProfile = (buEyeBaseVer5.Apps.buProfileCalc) null;
+  public static buFlexoCalc cFlexo = (buFlexoCalc) null;
+  public static buNestingCalc cNesting = (buNestingCalc) null;
+  public static buDrillCalc cDrill = (buDrillCalc) null;
+  public static buSewingCalc cSewing = (buSewingCalc) null;
+  public static buFoamCalc cFoamCut = (buFoamCalc) null;
+  public static buPipeBendCalc cPipeBend = (buPipeBendCalc) null;
+  public static buPrinter3D cPrinter3D = (buPrinter3D) null;
+  public static buRouter3AX cRouter3AX = (buRouter3AX) null;
+  public static buRollerBendCalc cRollerBend = (buRollerBendCalc) null;
+  public static buToolGrindingCalc cToolGrind = (buToolGrindingCalc) null;
+  public static buWood cWood = (buWood) null;
+  public static buDoor cDoor = (buDoor) null;
+  public static buRoboticCalc cRobotic = (buRoboticCalc) null;
+  public static buPanelCutCalc cPanelCut = (buPanelCutCalc) null;
+  public static buDiamakerCalc cDiamaker = (buDiamakerCalc) null;
+  public static buAppCalc cAppCalc = (buAppCalc) null;
+  public static buSort cSort = (buSort) null;
+  public static buNet cNet = (buNet) null;
+  public static buString cString = (buString) null;
+  public static buGeneral cGeneral = (buGeneral) null;
+  public static buNumeric cNumeric = (buNumeric) null;
+  public static buImage cImage = (buImage) null;
+  public static buMatrix cMatrix = (buMatrix) null;
+  public static buGCodeCreate cGcodeCreate = (buGCodeCreate) null;
+  public static buCam5 cCam5 = (buCam5) null;
+  public static buVector5 cVector5 = (buVector5) null;
+  public static buFile5 cFile5 = (buFile5) null;
+  public static buKinematic5 cKinematic5 = (buKinematic5) null;
+  public static buMatrix5 cMatrix5 = (buMatrix5) null;
+  public static buControlCommands cControl = (buControlCommands) null;
+
+  public static void Init()
+  {
+    clsInit.appFiles = new clsFiles();
+    clsInit.appCommand = new clsCommand();
+    clsInit.appSystem = new clsSystem();
+    buControlCommands buControlCommands = new buControlCommands();
+  }
 }
